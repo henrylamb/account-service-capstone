1c1b0e29ada8638e38758b9bbf83f0917a30b4c5
wasadmin <wasadmin@ip-172-31-37-95.ap-south-1.compute.internal>
2024-10-18 14:34:47 +0000

    Created PUT request

--- src/main/java/com/adp/controller/UserController.java
+++ src/main/java/com/adp/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.adp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +32,15 @@ public class UserController {
           return repo.findById(id);
     }  
     
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable("id") long id) {
+        if (user.getId()!=id || user.getName()!=null || user.getPassword()!=null ||
+        user.getAddress()!=null || user.getPhone()!=null || user.getResume()!=null ||
+        user.getDepartment()!=null || user.getRole()!=null) {
+            return ResponseEntity.badRequest().build();
+        }
+        repo.save(user);
+        return ResponseEntity.ok().build();
+        
+    } 
 }
