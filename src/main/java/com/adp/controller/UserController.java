640d28842167eb107e32f0e6221b9f5ff56b0d6f
Antonia <antonia.vintur@adp.com>
2024-10-18 14:42:52 +0100

    Added GET requests

--- src/main/java/com/adp/controller/UserController.java
+++ src/main/java/com/adp/controller/UserController.java
@@ -2,14 +2,19 @@ package com.adp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 import com.adp.domain.User;
 import com.adp.repository.UserRepository;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     
     @Autowired UserRepository repo;
@@ -17,6 +22,11 @@ public class UserController {
     @GetMapping
     public Iterable<User> getAll() {
         return repo.findAll();
-      }
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable("id") long id) {
+          return repo.findById(id);
+    }  
     
 }
