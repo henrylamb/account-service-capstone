079721c40a41fe879dead6001ba9753439a92a20
Antonia <antonia.vintur@adp.com>
2024-10-18 13:35:05 +0100

    User domain created

--- src/main/java/com/adp/domain/User.java
+++ src/main/java/com/adp/domain/User.java
@@ -8,18 +8,78 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="USERS")
+@Table(name="USER")
 public class User {
-  @Id
+  	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	
-	@Column(name="username")
+	@Column(name="FULL_NAME")
 	private String name;
 	
 	private String password;
 	
 	private String email;
+
+	private String address;
+
+	private String phone;
+
+	private String resume;
+
+	private String department;
+
+	private String role;
+
+	private long admin_id;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getResume() {
+		return resume;
+	}
+
+	public void setResume(String resume) {
+		this.resume = resume;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public long getAdmin_id() {
+		return admin_id;
+	}
+
+	public void setAdmin_id(long admin_id) {
+		this.admin_id = admin_id;
+	}
 
 	public long getId() {
 		return id;
