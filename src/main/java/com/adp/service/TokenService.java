7d29c0c3c484431a8c648e8146810c1938a77ab3
Antonia <antonia.vintur@adp.com>
2024-10-21 15:30:49 +0100

    Change validation by email instead of name

--- src/main/java/com/adp/service/TokenService.java
+++ src/main/java/com/adp/service/TokenService.java
@@ -32,9 +32,10 @@ public class TokenService {
   }
   
   public boolean validateUser(User user) {
-    if (user.getName() != null && user.getPassword() != null) {
-      Optional<User> optUser = getUser(user.getName());
-      return (optUser.isPresent() && optUser.get().getPassword().equals(user.getPassword()));
+    if (user.getEmail() != null && user.getPassword() != null) {
+      Optional<User> optUser = getUser(user.getEmail());
+      return (optUser.isPresent() && optUser.get().getPassword().equals(user.getPassword())
+      && optUser.get().getEmail().equals(user.getEmail()) );
     }
     return false;
   }
