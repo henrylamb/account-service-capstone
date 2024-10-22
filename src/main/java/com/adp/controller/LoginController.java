46c785299b1f67e6e2221ad52c8cf9e3d5118367
Antonia <antonia.vintur@adp.com>
2024-10-22 10:20:58 +0100

    Added cookie to login endpoint

--- src/main/java/com/adp/controller/LoginController.java
+++ src/main/java/com/adp/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.adp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.adp.domain.User;
 import com.adp.dto.Token;
@@ -18,14 +22,18 @@ public class LoginController {
   TokenService tokenService;
 
   @PostMapping
-  public ResponseEntity<?> getToken(@RequestBody User user){
+  public ResponseEntity<?> getToken(@RequestBody User user, HttpServletResponse response){
     boolean isValid = tokenService.validateUser(user);
     if (isValid) {
       Token token = tokenService.generateToken(user);
-      
-      return ResponseEntity.ok(token);
+      System.out.println(token);
+      Cookie cookie = new Cookie("user", token.getToken());
+      cookie.setHttpOnly(true);
+      cookie.setPath("/");
+      response.addCookie(cookie);
+      return ResponseEntity.ok().build();
     } else {
-      return ResponseEntity.status(401).body("Invalid credentials");
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
   }
 }
