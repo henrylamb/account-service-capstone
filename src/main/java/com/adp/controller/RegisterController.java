9f773c2cb8d6e09ea4b2ed15c654c85db31e0e58
wasadmin <wasadmin@ip-172-31-37-95.ap-south-1.compute.internal>
2024-10-22 09:57:50 +0000

    cookies Registration

--- src/main/java/com/adp/controller/RegisterController.java
+++ src/main/java/com/adp/controller/RegisterController.java
@@ -7,27 +7,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
 import com.adp.domain.User;
 import com.adp.dto.Token;
 import com.adp.service.RegisterService;
 import com.adp.service.TokenService;
 
 
+
 @RestController
 @RequestMapping("/registration")
 public class RegisterController {
+  
 
   @Autowired RegisterService registerService;
   @Autowired TokenService tokenService;
 
   @PostMapping
-  public ResponseEntity<?> userRegistration(@RequestBody User user){
+  public ResponseEntity<?> userRegistration(@RequestBody User user, HttpServletResponse response){
     if(registerService.saveCustomer(user)){
       Token token = tokenService.generateToken(user);
-      return ResponseEntity.ok(token);
+      Cookie cookie = new Cookie("user", token.getToken());
+      cookie.setHttpOnly(true);
+      cookie.setPath("/");
+      response.addCookie(cookie);
+      
+      return ResponseEntity.ok().build();
 
     }
-    return ResponseEntity.badRequest().body("Register fail");
+    return ResponseEntity.badRequest().build();
   }
 
   
